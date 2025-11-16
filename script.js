4aa87acaee7b49a59128a53dc4116dd61062b8ed
makaka119911-oss <makaka119911@gmail.com>
2025-11-16 16:15:45 +0300

    Update script.js
    fix

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const TELEGRAM_BOT_TOKEN = '8402206062:AAEJim1GkriKqY_o1mOo0YWSWQDdw5Qy2h0';
 const TELEGRAM_CHAT_ID = '-1002313355102';
 const ARCHIVE_PASSWORD = 'admin123'; // Пароль для архива
-
+const API_BASE_URL = 'https://tatiana-server-production.up.railway.app/api';
 // Глобальные переменные для теста
 let currentStep = 1;
 let totalSteps = 7;
